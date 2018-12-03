46726b70c2abc5b31514a6e598c3d6b6e745293e
inbalaltan <44667893+inbalaltan@users.noreply.github.com>
2018-12-03 16:12:42 +0200

    Add files via upload

---
+++ Html/CheckersInstruction.php
@@ -0,0 +1,25 @@
+<?php
+
+$servername = "sql102.epizy.com";
+$username = "epiz_23059576";
+$password = "34821317";
+$dbname = "epiz_23059576_DB";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT* FROM Rules WHERE Nameofgame='Checkers'";
+
+$result = $conn->query($sql);
+
+$row = $result->fetch_assoc();
+$GameName=$row["Nameofgame"];
+$GameData=$row["Rules"];
+$Points=$row["Points"];
+
+$conn->close();
+?> 
