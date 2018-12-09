0fdd547335fe6353c61e458f6420e3b82370091b
Sean1511 <44668072+Sean1511@users.noreply.github.com>
2018-12-09 12:11:57 +0200

    Add files via upload
    new delete user option
    new reports screen
    update reg file

---
+++ reg.php
@@ -0,0 +1,50 @@
+<?php include('connect.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<title>Registertion</title>
+<link rel="stylesheet" href="/Css/reg.css">
+<body>
+<form action="reg.php" method="post">
+  <div class="container">
+    <h1 align="center">Register</h1>
+    <p align="center">Please fill in this form to create an account</p>
+    <hr>
+    <div class="form-contain">
+      <div class="form-left">
+        <label for="First Name"><b>First Name</b></label>
+        <input type="text" placeholder="First Name" name="FirstName" required>
+  
+        <label for="Last Name"><b>Last Name</b></label>
+        <input type="text" placeholder="Last Name" name="LastName" required>
+  
+        <label for="Age"><b>Age</b></label>
+        <input type="text" placeholder="Age" name="Age" required>
+  
+        <label for="Country"><b>Country</b></label>
+        <input type="text" placeholder="Country" name="Country" required>
+      </div>
+      <div class="form-right">
+        <label for="username"><b>User Name</b></label>
+        <input type="text" placeholder="Enter User Name" name="username" required>
+      
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+      
+        <label for="pswr"><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name="pswr" required>
+      </div>
+    </div>
+    <hr>
+    <button type="submit" name="register" class="registerbtn">Register</button>
+  </div>
+  <div class="container signin">
+    <p>Already have an account? <a href="/Html/LoginU.php">Sign in</a>.</p>
+    <p><a href="/Html/Home.html">Go Back</a></p>
+  </div>
+</form>
+
+</body>
+</html>
